46d53e749581abd3b097badce484cf468f7d2816
Bha108jain <bhavyajainn1008@gmail.com>
2025-03-23 01:47:36 +0530

    fixing issue

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -20,7 +20,7 @@ function App() {
 
   async function reviewCode() {
     const response = await axios.post(
-      `${import.meta.env.REACT_APP_BACKEND_URL}/ai/get-review`,
+      `${import.meta.env.VITE_APP_BACKEND_URL}/ai/get-review`,
       {
         code,
       }
