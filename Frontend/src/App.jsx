012cc6cd4fecbf76b1dd8d801e444de34ca7515e
Bha108jain <bhavyajainn1008@gmail.com>
2025-03-26 23:39:17 +0530

    production build

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -20,7 +20,7 @@ function App() {
 
   async function reviewCode() {
     const response = await axios.post(
-      `${import.meta.env.VITE_APP_BACKEND_URL}/ai/get-review`,
+      `https://syntax-iq-ol8s.vercel.app/ai/get-review`,
       {
         code,
       }
