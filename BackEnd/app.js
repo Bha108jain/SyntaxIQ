39325dffad6edb72ba7a720fcea484afe1d7b4ba
Bha108jain <bhavyajainn1008@gmail.com>
2025-03-26 22:08:04 +0530

    vercel

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -9,7 +9,12 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
-
+app.get("/", (req, res) => {
+  res.send({
+    activeStatus: true,
+    error: false,
+  });
+});
 app.use("/ai", aiRoutes);
 
 module.exports = app;
